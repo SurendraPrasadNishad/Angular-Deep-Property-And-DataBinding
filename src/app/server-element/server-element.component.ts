e98bed99961e0c09636cb4b5fd06168d80dd9a86
SurendraPrasadNishad <surendraprasadnishad123@gmail.com>
2023-11-14 23:00:47 +0545

    View Encapsulation css

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,9 +1,14 @@
-import { Component, OnInit,Input } from '@angular/core';
+import { Component, OnInit,Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+  encapsulation:ViewEncapsulation.None
+  //When particular comnponent css applied to particular component html ie:default(Emulated)
+  //then it work only for that particular component but if put (ViewEncapsulation.None) then it applied gobally
+  //but if applied (ViewEncapsulation.Native or shadowDom) then work for only that particular component on 
+  //the basic of native browser
 })
 export class ServerElementComponent implements OnInit {
   //js object for type defination
