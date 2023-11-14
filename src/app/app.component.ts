f61f11b92cfcfa2085c66553214b3758f8e044b1
SurendraPrasadNishad <surendraprasadnishad123@gmail.com>
2023-11-14 16:41:52 +0545

    Pass data between component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,27 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'LearnComponentAndDataBindDeep';
+  title='LearnComponentAndDataBindDeep';
+  serverElements = [{type:'server',name:'TestServer',content:'This is a test server'}];
+
+ //from cockpit getting ServerName and ServerContent ie:from inside componenmt to outside component
+onServerAdded(serverData:{serverName:string,serverContent:string}) {
+  this.serverElements.push({
+    type: 'server',
+    name: serverData.serverName,
+    content: serverData.serverContent
+  });
 }
+
+onBlueprintAdded(blueprintData:{serverName:string,serverContent:string}) {
+this.serverElements.push({
+    type: 'blueprint',
+    name: blueprintData.serverName,
+    content: blueprintData.serverContent
+});
+}
+}
+
+
---
+++ src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit ,EventEmitter,Output} from '@angular/core';
+import { NgModule } from '@angular/compiler/src/core';
+import { NgModel } from '@angular/forms';
+
+@Component({
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html',
+  styleUrls: ['./cockpit.component.css'],
+  providers:[NgModel]
+})
+export class CockpitComponent implements OnInit {
+  //making custom event serverCreated so making properties serverCreated and blueprintCreated
+  //new EventEmitter<> make event imported from @angular/core'; it is generic type so <{eventData}>
+  //for calling constructor of the eventemitter() which created and store  on serverCreated
+  //for passing out of component use decorator @Output()
+ @Output('serCreated') serverCreated=new EventEmitter<{serverName:string,serverContent:string}>();
+ @Output('bpCreated') blueprintCreated=new EventEmitter<{serverName:string,serverContent:string}>();;
+  newServerName = '';
+  newServerContent = '';
+
+  constructor() { 
+    
+  }
+
+  ngOnInit(): void {
+  }
+  onAddServer(){
+    //emit new event of serverCreated type and pass object emit(object)
+    this.serverCreated.emit({serverName:this.newServerName,serverContent:this.newServerContent});
+  }
+  onAddBlueprint(){
+    //emit new event of serverCreated type and pass object emit(object)
+    this.blueprintCreated.emit({serverName:this.newServerName,serverContent:this.newServerContent});
+  }
+   }
+
+
